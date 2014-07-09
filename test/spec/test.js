7a1209d9f84679d2aa1bda31d06a3c1ccddcc5c9
maswhitaker <masforwhi@yahoo.com>
2014-07-09 09:42:19 -0400

    Made tests universal

--- test/spec/test.js
+++ test/spec/test.js
@@ -9,14 +9,14 @@
 
         describe('_.pluck', function () {
           it("should expect _.pluck to create an array of ages", function(){
-            expect(_.pluck(people,'age')).to.deep.equal([18,17,11]);
+            expect(_.pluck(people,'age')).to.deep.equal([people[0].age,people[1].age,people[2].age]);
           });
           it("should expect ages to be an equal _.pluck", function (){
             var ages = _.pluck(people,'age');
             expect(ages).to.deep.equal(_.pluck(people,'age'));
           });
           it("should expect _.pluck to create an array of weights",function(){
-            expect(_.pluck(people,'weight')).to.deep.equal([170,100,85]);
+            expect(_.pluck(people,'weight')).to.deep.equal([people[0].weight,people[1].weight,people[2].weight]);
           });
           it("should expect weights to be an equal to the plucked data",function(){
             var weights = _.pluck(people, 'weight');
@@ -29,12 +29,10 @@
 
         describe('_.first',function (){
           it("should take the first age in the array",function(){
-            var ages = _.pluck(people,'age');
-            expect(_.first(ages)).to.equal(18);
+            expect(_.first(_.pluck(people,'age'))).to.equal(people[0].age);
           });
           it("should take the first weight in the array",function(){
-            var weights = _.pluck(people,'weight');
-            expect(_.first(weights)).to.equal(170);
+            expect(_.first(_.pluck(people,'weight'))).to.equal(people[0].weight);
           });
           it("should expect that when no argument is entered into _.first() it will result in undefined",function(){
             expect(_.first()).to.equal(undefined);
@@ -45,13 +43,11 @@
         });
 
         describe("_.last",function(){
-          it("should take the last age in the array",function(){
-            var ages = _.pluck(people,'age');
-            expect(_.last(ages)).to.equal(11);
+          it("should expect that the output is the last age in the array",function(){
+            expect(_.last(_.pluck(people,'age'))).to.equal(people[people.length-1].age);
           });
-          it("should take the last weight in the array",function(){
-            var weights = _.pluck(people,'weight');
-            expect(_.last(weights)).to.equal(85);
+          it("should expect that the output is the last weight in the array",function(){
+            expect(_.last(_.pluck(people,'weight'))).to.equal(people[people.length-1].weight);
           });
           it("should expect that when no argument is entered into _.last() it will result in undefined",function(){
             expect(_.last()).to.equal(undefined);
@@ -63,10 +59,10 @@
 
         describe("_.size",function(){
           it("should expect that when you call _.size on an array it will give you the number of items in the array",function(){
-            expect(_.size(people)).to.equal(3);
+            expect(_.size(people)).to.equal(people.length);
           });
           it("should expect that when you call _.size on a string it will return the amount of characters that string contains",function(){
-            expect(_.size("boop")).to.equal(4);
+            expect(_.size("boop")).to.equal("boop".length);
           });
           it("should expect that when you call _.size on a boolean it will return 0",function(){
             expect(_.size(true)).to.equal(0);
@@ -75,20 +71,16 @@
 
         describe("_.contains",function(){
           it("should expect that when you call _.contains on an array of numbers and search for that number. If the array contains it, it will output a boolean true",function(){
-            var ages = _.pluck(people,'age');
-            expect(_.contains(ages, 17)).to.equal(true);
+            expect(_.contains((_.pluck(people,'age')), 17)).to.equal(true);
           });
           it("should expect that when you call _.contains on an array of numbers and search for that number. If the array does not contain it, it will output a boolean false",function(){
-            var ages = _.pluck(people,'age');
-            expect(_.contains(ages,99)).to.equal(false);
+            expect(_.contains((_.pluck(people,'age')),99)).to.equal(false);
           });
           it("should expect that when you call _.contains on an array of names and search for a specific name. If the array contains the name, it will output a boolean true.",function(){
-            var names = _.pluck(people,'name');
-            expect(_.contains(names,'mason'))
+            expect(_.contains((_.pluck(people,'name')),'mason')).to.equal(true);
           });
           it("should expect that when you call _.contains on an array of names and search for a specific name. If the array does not contain the name, it will output a boolean false.",function(){
-            var names = _.pluck(people,'name');
-            expect(_.contains(names,'jack')).to.equal(false);
+            expect(_.contains((_.pluck(people,'name')),'jack')).to.equal(false);
           });
         });
         describe("_.random",function(){
@@ -122,8 +114,7 @@
 
 
 })();
-//
-// (function (){
+
 //   (function(){
 //   _ = {};
 //   _.pluck = function(obj,key){
@@ -145,5 +136,4 @@
 //       });
 //     });
 //   });
-// })()
 // })();
